adc452c67468e870481896eaf7aae9f8dd05c050
Emily Gong <emily.f.gong@gmail.com>
2018-11-30 22:23:11 -0500

    Made changes to the modal button

--- assignment-8/js/content.js
+++
@@ -1,5 +0,0 @@
-const cardData = [
-  { title: 'this is a tile', imagePath: 'this is imagePath', content: 'this is content', type: 'good'},
-  { title: 'this is a tile', imagePath: 'this is imagePath', content: 'this is content', type: 'good'},
-  { title: 'this is a tile', imagePath: 'this is imagePath', content: 'this is content', type: 'bad'},
-]
